9dce52aaac9463d41ae5f64c9695a4f94ad98aff
adarsh-nl <47457585+adarsh-nl@users.noreply.github.com>
2023-05-13 11:55:00 +0530

    Update RLHF_HuamnCaption_HumanRating.py

--- RLHF_HuamnCaption_HumanRating.py
+++ RLHF_HuamnCaption_HumanRating.py
@@ -24,6 +24,54 @@ gamma = 0.9 # Discount factor
 q_table = np.random.rand(max_length, len(tokenizer.word_index) + 1)
 RL_learning_loop = 0
 
+def extract_features(filename, model):
+        try:
+            image = Image.open(filename)
+
+        except:
+            print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
+        image = image.resize((299,299))
+        image = np.array(image)
+        # for images that has 4 channels, we convert them into 3 channels
+        if image.shape[2] == 4: 
+            image = image[..., :3]
+        image = np.expand_dims(image, axis=0)
+        image = image/127.5
+        image = image - 1.0
+        feature = model.predict(image)
+        return feature
+
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+def sample_pred(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    preds = preds.flatten() # flatten the array to make it 1D
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'start'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        preds = model.predict([photo, sequence], verbose=0)
+        preds = preds.reshape((preds.shape[1],))
+        next_index = sample_pred(preds, temperature=0.5) #adjust the temperature parameter to control the randomness of predictions
+        word = word_for_id(next_index, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'end':
+            break
+    return in_text
+
 def custom_loss(cross_entropy, feedback):
   custom_loss = cross_entropy - feedback
   return custom_loss
